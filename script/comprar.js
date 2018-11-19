4d0ea4e780d7e9b5ca289205c6ee00a0e3e767f7
emilianogd <emilianogonzalez9@hotmail.com>
2018-11-19 20:03:28 -0300

    Cambio en el codigo, uso de bootstrap, lo que se borro mucho css, y anexo de javascript.

---
+++ script/comprar.js
@@ -0,0 +1,117 @@
+var DatosJuegos = {
+    "Juegos" : [{
+        "nombre" : "Call of Duty - MW3",
+        "value" : "Juego1",
+        "precio" : 950
+    },{ 
+        "nombre" : "Battlefield V",
+        "value" : "Juego2",
+        "precio" : 1250
+    },{ 
+        "nombre" : "Grand Theft Auto V",
+        "value" : "Juego3",
+        "precio" : 1500
+    },{ 
+        "nombre" : "Skyrim",
+        "value" : "Juego4",
+        "precio" : 650
+    },{
+        "nombre" : "Megaman",
+        "value" : "Juego5",
+        "precio" : 750
+    },{
+        "nombre" : "Aleatorio",
+        "value" : "Juego6",
+        "precio" : 500
+    }]
+}
+
+var selectElement = document.getElementById("selectJuego");
+var listaJuegos = DatosJuegos.Juegos;
+var option;
+
+for (let i = 0; i < listaJuegos.length; i++) {
+    const juego = listaJuegos[i];
+
+    let option = document.createElement("option");
+    option.value = juego.value;
+    option.innerHTML = juego.nombre;
+    selectElement.appendChild(option);
+}
+
+var cantidadElement = document.getElementById("cantidadJuegos");
+cantidadElement = function (){//Compara el juego seleccionada con los de la base de datos anterior..
+    
+    if(document.getElementById("selectJuego").value==="Juego1"){
+        var precioid = DatosJuegos.Juegos[0].precio*document.getElementById("cantidadJuegos").value;
+        document.getElementById("calculoSinI").innerHTML = precioid;
+
+    }else if(document.getElementById("selectJuego").value==="Juego2"){
+        var precioid = DatosJuegos.Juegos[1].precio*document.getElementById("cantidadJuegos").value;
+        document.getElementById("calculoSinI").innerHTML = precioid;
+    
+    }else if(document.getElementById("selectJuego").value==="Juego3"){
+        var precioid = DatosJuegos.Juegos[2].precio*document.getElementById("cantidadJuegos").value;
+        document.getElementById("calculoSinI").innerHTML = precioid;
+    
+    }else if(document.getElementById("selectJuego").value==="Juego4"){
+        var precioid = DatosJuegos.Juegos[3].precio*document.getElementById("cantidadJuegos").value;
+        document.getElementById("calculoSinI").innerHTML = precioid;
+    
+    }else if(document.getElementById("selectJuego").value==="Juego5"){
+        var precioid = DatosJuegos.Juegos[4].precio*document.getElementById("cantidadJuegos").value;
+        document.getElementById("calculoSinI").innerHTML = precioid;
+    
+    }else if(document.getElementById("selectJuego").value==="Juego6"){
+        var precioid = DatosJuegos.Juegos[5].precio*document.getElementById("cantidadJuegos").value;
+        document.getElementById("calculoSinI").innerHTML = precioid;
+    }
+
+    if(document.getElementById("selectFormato").value === "fisico"){//Verifica el tipo de envio segun la opcion.
+        if(document.getElementById("selectEnvio").value === "comun"){
+            var envio = precioid * 0.005;
+            document.getElementById("precioEnvio").innerHTML = envio;
+            var precioParcial = envio + precioid;
+            var iva = precioParcial * 0.22;
+            document.getElementById("precioIVA").innerHTML = iva;
+            var precioTotal = iva + envio + precioid;
+            document.getElementById("precioTotal").innerHTML = precioTotal;
+        }
+        else if(document.getElementById("selectEnvio").value === "especial"){
+            var envio = precioid * 0.02;
+            document.getElementById("precioEnvio").innerHTML = envio;
+            var precioParcial = envio + precioid;
+            var iva = precioParcial * 0.22;
+            document.getElementById("precioIVA").innerHTML = iva;
+            var precioTotal = iva + envio + precioid;
+            document.getElementById("precioTotal").innerHTML = precioTotal;
+        }
+        else if(document.getElementById("selectEnvio").value === "premium"){
+            var envio = precioid * 0.05;
+            document.getElementById("precioEnvio").innerHTML = envio;
+            var precioParcial = envio + precioid;
+            var iva = precioParcial * 0.22;
+            document.getElementById("precioIVA").innerHTML = iva;
+            var precioTotal = iva + envio + precioid;
+            document.getElementById("precioTotal").innerHTML = precioTotal;
+        }
+    }
+    else if(document.getElementById("selectFormato").value === "digital"){
+        document.getElementById("precioEnvio").innerHTML = 0;    
+        var precioParcial = precioid;
+        var iva = precioParcial * 0.22;
+        document.getElementById("precioIVA").innerHTML = iva;   
+        var precioTotal = iva + precioid;
+        document.getElementById("precioTotal").innerHTML = precioTotal;
+    }
+
+}
+
+function mostrar(id) {
+    if (id == "fisico") {
+        $("#fisico").show();
+    }//Si el juego es fisico se muestra un nuevo desplegable con opciones de envio.
+    if(id == "digital"){
+        $("#fisico").hide();
+    }//Si el juego es digital no mustra las opciones de envio.
+}
---
+++ script/crearCuenta.js
@@ -0,0 +1,91 @@
+
+/*Funcion Cargar*/
+$(document).ready(function(){    
+    $('#btnCargar').click(function(){                       
+        /*Obtener datos almacenados*/
+        var email = localStorage.getItem("Email");
+        var contraseña  = localStorage.getItem("Contraseña");
+    });   
+});
+
+function validar(){
+    var nombre, apellido, email, celular, contrasenia, recontrasenia;
+    nombre = document.getElementById("Nombre").value;//Asigno el dato ingresado en la variable correspondiente.
+    apellido = document.getElementById("Apellido").value;//Asigno el dato ingresado en la variable correspondiente.
+    email = document.getElementById("Email").value;//Asigno el dato ingresado en la variable correspondiente.
+    celular = document.getElementById("Celular").value;//Asigno el dato ingresado en la variable correspondiente.
+    contrasenia = document.getElementById("Contraseña").value;//Asigno el dato ingresado en la variable correspondiente.
+    recontrasenia = document.getElementById("reContraseña").value;//Asigno el dato ingresado en la variable correspondiente.
+
+    /*Guardando los datos en el LocalStorage*/
+    localStorage.setItem("Nombre", nombre);
+    localStorage.setItem("Apellido", apellido);
+    localStorage.setItem("Email", email);
+    localStorage.setItem("Celular", celular);
+    localStorage.setItem("Contraseña", contrasenia);
+    localStorage.setItem("reContraseña", recontrasenia);
+
+
+    if(nombre=== ""){//Validar el nombre no este vacio.
+        alert("Nombre vacio");
+        return false;
+    }else if(apellido === ""){//Validar el apellido no este vacio.
+        alert("Apellido vacio");    
+        return false;
+    }else if(email === ""){//Validar el email no este vacio.
+        alert("Email vacio");    
+        return false;
+    }else if(celular === ""){//Validar el celular no este vacio.
+        alert("Celular vacio");    
+        return false;
+    }else if(contrasenia === ""){//Validar que la contraseña no este vacia.
+        alert("Contraseña vacio");    
+        return false;
+    }else if(recontrasenia === ""){//Validar que repetir contraseña no este vacia.
+        alert("Repetir Contraseña vacio");    
+        return false;
+    }else if(contrasenia !== recontrasenia){//Validar que las contraseñas ingresadas sean iguales.
+        alert("Las contreseñas no coinciden");
+        return false;
+    }
+    alert("¡Registro Exitoso!");
+}
+
+
+function validarInicio(){
+    if(document.getElementById("Usuario").value === ""){//Verifica que el usuario no ese vacio.
+        alert("Usuario Vacio");
+        return false;
+    }else if(document.getElementById("Usuario").value !== localStorage.Email){//Verifica que el usuario sea correcto.
+        alert("El usuario no coincide con el registrado");
+        return false;
+    }else if(document.getElementById("Password").value === ""){//Verifica que la contraseña no sea vacia.
+        alert("Contraseña Vacia");
+        return false;
+    }else if(document.getElementById("Password").value !== localStorage.Contraseña){//Verifica la contraseña sea correcta.
+        alert("Las contreseñas no coinciden");
+        return false;    
+    }
+    alert("¡Bienvenido!");
+    
+    
+}
+
+function validarCompra(){
+    if(document.getElementById("compraNombre").value === ""){//Verifica que el usuario no ese vacio.
+        alert("Nombre Vacio");
+        return false;
+    }else if(document.getElementById("compraEmail").value === ""){//Verifica que el email no ese vacio.
+        alert("Email Vacio");
+        return false;
+    }else if(document.getElementById("compraNombre").value !== localStorage.Nombre){//Verifica que el usuario sea el correcto.
+        alert("El nombre no coincide con el registrado");
+        return false;
+    }else if(document.getElementById("compraEmail").value !== localStorage.Email){//Verifica que el email sea el correcto.
+        alert("El email no coincide con el registrado");
+        return false;
+    }
+    alert("Compra Exitosa");
+
+}
+
